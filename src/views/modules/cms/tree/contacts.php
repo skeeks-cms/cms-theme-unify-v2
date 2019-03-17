633e7b352fcf080982b2554c64341308eb43569e
semenov <semenov@skeeks.com>
2019-03-17 22:10:36 +0300

    dev

---
+++ src/views/modules/cms/tree/contacts.php
@@ -0,0 +1,83 @@
+<!-- Promo Block -->
+<section>
+    <!-- Parallax Image -->
+    <div style="height: 500px;">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3c54488d4f32ae571f2430a060822efc953527a3b093179c979cefdc898476ad&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=false"></script>
+    </div>
+    <!-- End Parallax Image -->
+
+</section>
+<section class="clearfix g-brd-bottom  g-bg-gray-dark-v1 g-color-white-opacity-0_9">
+    <!-- Icons Block -->
+    <div class="row no-gutters g-py-60">
+        <div class="col-lg col-md-6 col-sm-6 col-xs-12 g-brd-right--md g-brd-gray-dark-v3">
+            <!-- Icon Blocks -->
+            <div class="text-center g-py-20">
+            <span class="u-icon-v1 u-icon-size--xl g-mb-10">
+
+                <i class="icon-real-estate-027 u-line-icon-pro"></i>
+
+              </span>
+                <h4 class="h5 g-font-weight-600 g-mb-5"><?= \Yii::t('app', 'Address'); ?></h4>
+                <span class="d-block"><?= \Yii::t('app', 'Moscow, Zelenograd, 2005-29'); ?></span>
+            </div>
+            <!-- End Icon Blocks -->
+        </div>
+
+        <div class="col-lg col-md-6 col-sm-6 col-xs-12 g-brd-right--md g-brd-gray-dark-v3">
+            <!-- Icon Blocks -->
+            <div class="text-center g-py-20">
+            <span class="u-icon-v1 u-icon-size--xl g-mb-10">
+                <i class="icon-electronics-005 u-line-icon-pro"></i>
+              </span>
+                <h4 class="h5 g-font-weight-600 g-mb-5"><?= \Yii::t('app', 'Phone number'); ?></h4>
+                <span class="d-block"><a href="tel:+74950057926">(+7 495) 005-79-26 </a></span>
+            </div>
+            <!-- End Icon Blocks -->
+        </div>
+
+        <div class="col-lg col-md-6 col-sm-6 col-xs-12 g-brd-right--md g-brd-gray-dark-v3">
+            <!-- Icon Blocks -->
+            <div class="text-center g-py-20">
+            <span class="u-icon-v1 u-icon-size--xl g-mb-10">
+
+                <i class="icon-communication-062 u-line-icon-pro"></i>
+
+              </span>
+                <h4 class="h5 g-font-weight-600 g-mb-5"><?= \Yii::t('app', 'Email'); ?></h4>
+                <span class="d-block"><a href="mailto:info@skeeks.com">info@skeeks.com</a></span>
+            </div>
+            <!-- End Icon Blocks -->
+        </div>
+
+        <div class="col-lg col-md-6 col-sm-6 col-xs-12 g-brd-right--md g-brd-gray-dark-v3">
+            <!-- Icon Blocks -->
+            <div class="text-center g-py-20">
+            <span class="u-icon-v1 u-icon-size--xl g-mb-10">
+
+                <i class="icon-hotel-restaurant-003 u-line-icon-pro"></i>
+
+              </span>
+                <h4 class="h5 g-font-weight-600 g-mb-5"><?= \Yii::t('app', 'Opening hours'); ?></h4>
+                <span class="d-block"><?= \Yii::t('app', 'Monday - Friday: 10:00 to 19:00'); ?></span>
+            </div>
+            <!-- End Icon Blocks -->
+        </div>
+
+        <div class="col-lg col-md-12 col-sm-12 col-xs-12">
+            <!-- Icon Blocks -->
+            <div class="text-center g-py-20">
+            <span class="u-icon-v1 u-icon-size--xl g-mb-10">
+
+                <i class="fas fa-user"></i>
+
+              </span>
+                <h4 class="h5 g-font-weight-600 g-mb-5"><?= \Yii::t('app', 'Customer cabinet'); ?></h4>
+                <span class="d-block"><a href="<?= \yii\helpers\Url::to(['/upa-home']); ?>"><?= \Yii::t('app', 'Sign in'); ?></a></span>
+            </div>
+            <!-- End Icon Blocks -->
+        </div>
+    </div>
+    <!-- End Icons Block -->
+</section>
+
