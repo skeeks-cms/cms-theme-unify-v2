04f4b99069f3d7b4416d434959b7091a1c6ea5c3
semenov <semenov@skeeks.com>
2019-07-25 23:11:39 +0300

    dev

--- src/admin/assets/UnifyAdminAsset.php
+++ src/admin/assets/UnifyAdminAsset.php
@@ -8,7 +8,12 @@
 
 namespace skeeks\cms\themes\unify\admin\assets;
 
+use skeeks\assets\unify\base\UnifyAppearAsset;
 use skeeks\assets\unify\base\UnifyAsset;
+use skeeks\assets\unify\base\UnifyFancyboxAsset;
+use skeeks\assets\unify\base\UnifyHsPopupAsset;
+use skeeks\assets\unify\base\UnifyHsScrollbarAsset;
+use skeeks\cms\themes\unify\assets\FontAwesomeAsset;
 use skeeks\sx\assets\Custom;
 use yii\bootstrap\BootstrapAsset;
 use yii\bootstrap\BootstrapPluginAsset;
@@ -19,8 +24,8 @@ use yii\web\YiiAsset;
 class UnifyAdminAsset extends UnifyAsset
 {
     public $css = [
-        '//fonts.googleapis.com/css?family=Open+Sans%3A400%2C300%2C500%2C600%2C700%7CPlayfair+Display%7CRoboto%7CRaleway%7CSpectral%7CRubik',
-        'https://use.fontawesome.com/releases/v5.5.0/css/all.css',
+        '//fonts.googleapis.com/css?family=Open+Sans%3A400%2C300%2C500%2C600%2C700%7CPlayfair+Display%7CRoboto%7CRaleway%7CSpectral%7CRubik&display=swap',
+        //'https://use.fontawesome.com/releases/v5.5.0/css/all.css',
 
 
         'assets/vendor/icon-awesome/css/font-awesome.min.css',
@@ -32,7 +37,7 @@ class UnifyAdminAsset extends UnifyAsset
         'admin-template/assets/vendor/hs-admin-icons/hs-admin-icons.css',
 
         'assets/vendor/animate.css',
-        'assets/vendor/malihu-scrollbar/jquery.mCustomScrollbar.min.css',
+        //'assets/vendor/malihu-scrollbar/jquery.mCustomScrollbar.min.css',
 
         'admin-template/assets/vendor/flatpickr/dist/css/flatpickr.min.css',
         'admin-template/assets/vendor/bootstrap-select/css/bootstrap-select.min.css',
@@ -40,14 +45,13 @@ class UnifyAdminAsset extends UnifyAsset
         'admin-template/assets/vendor/chartist-js/chartist.min.css',
         'admin-template/assets/vendor/chartist-js-tooltip/chartist-plugin-tooltip.css',
 
-        'assets/vendor/fancybox/jquery.fancybox.min.css',
         'assets/vendor/hamburgers/hamburgers.min.css',
         'admin-template/assets/css/unify-admin.css',
         'assets/css/custom.css',
     ];
     public $js = [
         //'admin-template/assets/vendor/jquery/jquery.min.js',
-        'admin-template/assets/vendor/jquery-migrate/jquery-migrate.min.js',
+        //'admin-template/assets/vendor/jquery-migrate/jquery-migrate.min.js',
 
         'assets/vendor/cookiejs/jquery.cookie.js',
 
@@ -64,27 +68,26 @@ class UnifyAdminAsset extends UnifyAsset
 
         'assets/vendor/jquery-ui/ui/widgets/datepicker.js',
 
-        'assets/vendor/appear.js',
+        //'assets/vendor/appear.js',
         'admin-template/assets/vendor/bootstrap-select/js/bootstrap-select.min.js',
         'admin-template/assets/vendor/flatpickr/dist/js/flatpickr.min.js',
-        'assets/vendor/malihu-scrollbar/jquery.mCustomScrollbar.concat.min.js',
+        //'assets/vendor/malihu-scrollbar/jquery.mCustomScrollbar.concat.min.js',
         'admin-template/assets/vendor/chartist-js/chartist.min.js',
         'admin-template/assets/vendor/chartist-js-tooltip/chartist-plugin-tooltip.js',
-        'admin-template/assets/vendor/fancybox/jquery.fancybox.min.js',
 
-        'assets/js/hs.core.js',
+        //'assets/js/hs.core.js',
         'admin-template/assets/js/components/hs.side-nav.js',
         'assets/js/helpers/hs.hamburgers.js',
         'admin-template/assets/js/components/hs.range-datepicker.js',
         'assets/js/components/hs.datepicker.js',
         'assets/js/components/hs.dropdown.js',
-        'assets/js/components/hs.scrollbar.js',
+        //'assets/js/components/hs.scrollbar.js',
         'admin-template/assets/js/components/hs.area-chart.js',
         'admin-template/assets/js/components/hs.donut-chart.js',
         'admin-template/assets/js/components/hs.bar-chart.js',
         'admin-template/assets/js/components/hs.pie-chart.js',
         'assets/js/helpers/hs.focus-state.js',
-        'admin-template/assets/js/components/hs.popup.js',
+        //'admin-template/assets/js/components/hs.popup.js',
 
         'assets/js/custom.js',
     ];
@@ -93,5 +96,10 @@ class UnifyAdminAsset extends UnifyAsset
         Custom::class,
         BootstrapAsset::class,
         BootstrapPluginAsset::class,
+        UnifyFancyboxAsset::class,
+        FontAwesomeAsset::class,
+        UnifyHsPopupAsset::class,
+        UnifyAppearAsset::class,
+        UnifyHsScrollbarAsset::class,
     ];
 }
