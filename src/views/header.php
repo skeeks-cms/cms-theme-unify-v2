031ff300433c38cad7094722b17966049bee67bc
Semenov <semenov@skeeks.com>
2020-03-19 15:34:19 +0300

    dev

--- src/views/header.php
+++ src/views/header.php
@@ -6,7 +6,6 @@
  * @author Semenov Alexander <semenov@skeeks.com>
  */
 /* @var $this yii\web\View */
-/*print_r(\Yii::$app->cloudshopApiClient->getCatalogApiMethod());die;*/
 ?>
 
 <? if (\Yii::$app->mobileDetect->isMobile) : ?>
