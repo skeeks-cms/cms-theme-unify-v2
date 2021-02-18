190720c322055d104ecd461b8cd6de5075998aa7
Semenov <semenov@skeeks.com>
2021-02-18 15:21:29 +0300

    dev

--- src/views/include/bottom-block.php
+++ src/views/include/bottom-block.php
@@ -6,23 +6,34 @@
  * @author Semenov Alexander <semenov@skeeks.com>
  */
 /* @var $this yii\web\View */
+$this->registerCss(<<<CSS
+.sx-bottom-second-page-block {
+    border: solid 10px #eee;
+    margin-top: 20px;
+    padding: 30px;
+    background: white;
+}
+
+
+CSS
+);
 ?>
 <? if ($this->theme->isShowBottomBlock) : ?>
-    <section class="g-brd-around g-brd-10 g-brd-gray-light-v4 g-pa-30 g-mt-20 g-bg-white">
+    <section class="sx-bottom-second-page-block">
         <div class="row">
             <div class="col-md-9 align-self-center">
                 <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::beginWidget('bottom-block-title'); ?>
-                <h3 class="h4">Остались вопросы?</h3>
+                <div class="h4 sx-head-block">Остались вопросы?</div>
                 <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::end(); ?>
 
                 <?php if(\Yii::$app->skeeks->site->cmsSitePhone) : ?>
-                    <p class="lead g-mb-20 g-mb-0--md"><i class="g-color-black icon-phone g-font-size-24 g-valign-middle g-color-primary g-mr-10"></i> Звоните по телефону:
+                    <p class="lead"><i class="g-color-black icon-phone g-font-size-24 g-valign-middle g-color-primary g-mr-10"></i> Звоните по телефону:
                         <a href="tel:<?= \Yii::$app->skeeks->site->cmsSitePhone->value; ?>" class="g-font-weight-400 g-color-primary--hover g-text-underline--none--hover" style="font-size: 16px;"><?= \Yii::$app->skeeks->site->cmsSitePhone->value; ?></a>
                     </p>
                 <?php endif; ?>
                 
                 <?php if(\Yii::$app->skeeks->site->cmsSiteEmail) : ?>
-                    <p class="lead g-mb-20 g-mb-0--md">
+                    <p class="lead">
                         <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::beginWidget('bottom-block-phone-icon'); ?>
                         <i class="g-color-black icon-envelope g-font-size-24 g-valign-middle g-color-primary g-mr-10"></i>
                         <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::end(); ?>
@@ -34,7 +45,7 @@
                     </p>
                 <?php endif; ?>
                 
-                <p class="lead g-mb-20 g-mb-0--md">
+                <p class="lead">
                     <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::beginWidget('bottom-block-bottom-icon'); ?>
                     <i class="g-color-black icon-question g-font-size-24 g-valign-middle g-color-primary g-mr-10"></i>
                     <? \skeeks\cms\cmsWidgets\text\TextCmsWidget::end(); ?>
