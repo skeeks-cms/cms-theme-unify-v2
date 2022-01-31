d7be043a4fb6a73c7ebf02bd830c26f6a69083e5
Semenov <semenov@skeeks.com>
2022-02-01 01:17:40 +0300

    dev

--- src/views/widgets/ContentElementsCmsWidget/_advantage-v1-item.php
+++ src/views/widgets/ContentElementsCmsWidget/_advantage-v1-item.php
@@ -14,9 +14,19 @@
     <!-- Icon Blocks -->
     <div class="u-info-v2-2 text-center sx-color-primary">
         <div class="u-info-v2-2__item sx-brd-primary g-px-20 g-pb-30">
-          <span class="u-icon-v1 u-icon-size--2xl g-line-height-1 g-pull-50x-up">
+            <?php if ($model->image) : ?>
+                <img src="<?php echo \Yii::$app->imaging->thumbnailUrlOnRequest($model->image ? $model->image->src : null,
+                    new \skeeks\cms\components\imaging\filters\Thumbnail([
+                        'w' => 0,
+                        'h' => 250,
+                        'm' => \Imagine\Image\ManipulatorInterface::THUMBNAIL_INSET,
+                    ]), $model->code
+                ); ?>" class="img-fluid"/>
+            <?php else: ?>
+                <span class="u-icon-v1 u-icon-size--2xl g-line-height-1 g-pull-50x-up">
             <i class="<?= $model->relatedPropertiesModel->getAttribute('icon'); ?>"></i>
           </span>
+            <?php endif; ?>
             <h3 class="h5 g-font-weight-600 g-mt-minus-35 g-mb-10"><?= $model->name; ?></h3>
             <p class="mb-0"><?= $model->description_short; ?></p>
         </div>
--- src/views/widgets/ContentElementsCmsWidget/_advantage-v2-item.php
+++ src/views/widgets/ContentElementsCmsWidget/_advantage-v2-item.php
@@ -11,12 +11,24 @@
 ?>
 
 
-<div class="col-md-6 col-lg-3 g-mb-30">
+<div class="col-md-6 col-lg-3 col-6 g-mb-30">
     <!-- Icon Blocks -->
     <div class="text-center">
-      <span class="u-icon-v2 u-icon-size--3xl g-brd-7 g-brd-primary g-color-primary g-rounded-50x g-mb-25">
-        <i class="<?= $model->relatedPropertiesModel->getAttribute('icon'); ?>"></i>
-      </span>
+        <?php if($model->image) : ?>
+            <img src="<?php echo \Yii::$app->imaging->thumbnailUrlOnRequest($model->image ? $model->image->src : null,
+                    new \skeeks\cms\components\imaging\filters\Thumbnail([
+                        'w' => 0,
+                        'h' => 250,
+                        'm' => \Imagine\Image\ManipulatorInterface::THUMBNAIL_INSET,
+                    ]), $model->code
+                ); ?>" class="img-fluid" />
+        <?php else: ?>
+            <span class="u-icon-v2 u-icon-size--3xl g-brd-7 g-brd-primary g-color-primary g-rounded-50x g-mb-25">
+            <i class="<?= $model->relatedPropertiesModel->getAttribute('icon'); ?>"></i>
+          </span>
+        <?php endif; ?>
+        
+      
       <h3 class="h5 g-color-black mb-25"><?= $model->name; ?></h3>
       <p class="g-color-gray-dark-v4 g-mb-0"><?= $model->description_short; ?></p>
     </div>
