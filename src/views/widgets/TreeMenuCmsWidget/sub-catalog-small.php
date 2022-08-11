82e87af5f2d53f179d45729eec81ebbd588abb08
Semenov <semenov@skeeks.com>
2022-08-12 01:55:19 +0300

    webp

--- src/views/widgets/TreeMenuCmsWidget/sub-catalog-small.php
+++ src/views/widgets/TreeMenuCmsWidget/sub-catalog-small.php
@@ -21,7 +21,7 @@
                             <img src="<?= \skeeks\cms\helpers\Image::getSrc(\Yii::$app->imaging->thumbnailUrlOnRequest($model->mainImage ? $model->mainImage->src : null,
                                 new \skeeks\cms\components\imaging\filters\Thumbnail([
                                     'w' => 120,
-                                    'h' => 100,
+                                    'h' => 120,
                                     'm' => \Imagine\Image\ManipulatorInterface::THUMBNAIL_INSET,
                                 ]), $model->code
                             )); ?>
