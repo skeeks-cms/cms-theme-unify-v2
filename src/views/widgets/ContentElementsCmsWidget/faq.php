8f80319fe40c09e33a473abe9954e7f13cbb1f6a
Semenov <semenov@skeeks.com>
2025-09-25 13:14:01 +0300

    faq

--- src/views/widgets/ContentElementsCmsWidget/faq.php
+++ src/views/widgets/ContentElementsCmsWidget/faq.php
@@ -7,6 +7,98 @@
  */
 /* @var $this   yii\web\View */
 /* @var $widget \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget */
+$this->registerCss(<<<CSS
+/*------------------------------------
+  Accordions
+------------------------------------*/
+.u-accordion__header {
+  padding: 0.71429rem 1.07143rem;
+}
+
+.u-accordion__body {
+  padding: 1.07143rem;
+}
+
+.u-accordion__control-icon i:nth-child(1) {
+  display: none;
+}
+
+.collapsed .u-accordion__control-icon i:nth-child(1) {
+  display: inline;
+}
+
+.u-accordion__control-icon i:nth-child(2) {
+  display: inline;
+}
+
+.collapsed .u-accordion__control-icon i:nth-child(2) {
+  display: none;
+}
+
+[aria-expanded="false"] .u-accordion__control-icon i:nth-child(1) {
+  display: inline;
+}
+
+[aria-expanded="false"] .u-accordion__control-icon i:nth-child(2) {
+  display: none;
+}
+
+[aria-expanded="true"] .u-accordion__control-icon i:nth-child(1) {
+  display: none;
+}
+
+[aria-expanded="true"] .u-accordion__control-icon i:nth-child(2) {
+  display: inline;
+}
+
+.u-accordion [class*="et-icon-"], .u-accordion-line-icon-pro {
+  position: relative;
+  top: 3px;
+}
+
+.u-accordion-color-primary .u-accordion__header [aria-expanded="true"] {
+  color: #72c02c !important;
+}
+
+.u-accordion-color-white .u-accordion__header [aria-expanded="true"] {
+  color: #fff !important;
+}
+
+.u-accordion-bg-primary .u-accordion__header [aria-expanded="true"] {
+  background-color: #72c02c !important;
+  border-color: #72c02c !important;
+}
+
+.u-accordion-bg-white .u-accordion__header [aria-expanded="true"] {
+  background-color: #fff !important;
+  border-color: #fff !important;
+}
+
+.u-accordion-brd-primary .u-accordion__header [aria-expanded="true"] {
+  border-color: #72c02c !important;
+}
+
+.u-accordion-brd-white .u-accordion__header [aria-expanded="true"] {
+  border-color: #fff !important;
+}
+
+#accordion .card {
+    margin-bottom: 1.5rem;
+}
+
+#accordion .card .sx-accordion-heading a {
+    padding: 1.5rem 2rem;
+    box-shadow: 0 5px 10px -6px rgba(0, 0, 0, 0.1);
+    color: var(--text-color);
+}
+#accordion .card .u-accordion__body {
+    padding: 1.5rem 2rem;
+}
+#accordion .card .u-accordion__body p:last-child {
+    margin-bottom: 0;
+}
+CSS
+);
 ?>
 
 <? if ($elements = $widget->dataProvider->query->all()) : ?>
@@ -15,7 +107,7 @@
         <? foreach ($elements as $model) : ?>
             <!-- Card -->
             <div class="card g-brd-none rounded g-mb-20 g-bg-secondary" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
-                <div id="accordion-heading-<?= $model->id; ?>" class="g-pa-0" role="tab">
+                <div id="accordion-heading-<?= $model->id; ?>" class="g-pa-0 sx-accordion-heading" role="tab">
                     <h5 class="mb-0">
 
                         <a class="collapsed d-flex justify-content-between u-shadow-v19 g-color-main g-text-underline--none--hover rounded g-px-30 g-py-20"
