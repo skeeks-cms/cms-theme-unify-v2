1c22b24e765fd9e5d8aeab12f0a7be21bb126276
Semenov <semenov@skeeks.com>
2025-09-26 12:37:47 +0300

    Сортировка коллекций по популярности

--- src/views/modules/cms/tree/home-default.php
+++ src/views/modules/cms/tree/home-default.php
@@ -28,61 +28,85 @@ $content = \skeeks\cms\models\CmsContent::find()->where(['code' => 'slide'])->on
 ]); ?>
 
 <?php
-  $widget = \skeeks\cms\cmsWidgets\tree\TreeCmsWidget::beginWidget('home-blocks');
-  $widget->descriptor->name = 'Блоки на главной';
-  $widget->viewFile = '@app/views/widgets/TreeMenuCmsWidget/home-blocks';
-  $widget::end(); 
-  ?>
+$widget = \skeeks\cms\cmsWidgets\tree\TreeCmsWidget::beginWidget('home-blocks');
+$widget->descriptor->name = 'Блоки на главной';
+$widget->viewFile = '@app/views/widgets/TreeMenuCmsWidget/home-blocks';
+$widget::end();
+?>
 
 <? if ($model->description_full) : ?>
-<div class="sx-section">
-    <div class="container sx-content">
-        <header class="g-mt-50">
-            <?=$model->description_full; ?>
-        </header>
+    <div class="sx-section">
+        <div class="container sx-content">
+            <header class="g-mt-50">
+                <?= $model->description_full; ?>
+            </header>
+        </div>
     </div>
-</div>
 <? endif; ?>
 
 <?php
-  $widget = \skeeks\cms\cmsWidgets\tree\TreeCmsWidget::beginWidget('home-sub-catalog-new');
-  $widget->descriptor->name = 'Подразделы на главной';
-  $widget->viewFile = '@app/views/widgets/TreeMenuCmsWidget/home-sub-catalog';
-  $widget::end(); ?>
+$widget = \skeeks\cms\cmsWidgets\tree\TreeCmsWidget::beginWidget('home-sub-catalog-new');
+$widget->descriptor->name = 'Подразделы на главной';
+$widget->viewFile = '@app/views/widgets/TreeMenuCmsWidget/home-sub-catalog';
+$widget::end(); ?>
 
-<?/*= trim(\skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
+<? /*= trim(\skeeks\cms\cmsWidgets\treeMenu\TreeMenuCmsWidget::widget([
     'namespace'       => 'home-sub-catalog',
     'viewFile'        => '@app/views/widgets/TreeMenuCmsWidget/home-sub-catalog',
     'treeParentCode'  => "services",
     'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_N,
-])); */?>
+])); */ ?>
 
 
+<?
+$contentFaq = \skeeks\cms\models\CmsContent::find()->where(['code' => 'advantage'])->one();
+?>
+<?= \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget::widget([
+    'enabledPaging'   => 'N',
+    'namespace'       => 'advantage',
+    'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_Y,
+    'content_ids'     => [
+        $contentFaq ? $contentFaq->id : "",
+    ],
+    'viewFile'        => '@app/views/widgets/ContentElementsCmsWidget/advantage',
+]); ?>
 
 
-        <?
-        $contentFaq = \skeeks\cms\models\CmsContent::find()->where(['code' => 'advantage'])->one();
-        ?>
-        <?= \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget::widget([
-            'enabledPaging'       => 'N',
-            'namespace'       => 'advantage',
-            'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_Y,
-            'content_ids'     => [
-                $contentFaq ? $contentFaq->id : "",
-            ],
-            'viewFile'        => '@app/views/widgets/ContentElementsCmsWidget/advantage',
-        ]); ?>
-
-    
+    <section class="sx-section">
+        <div class="container sx-container">
+            <?= $this->render("@app/views/include/bottom-block"); ?>
+        </div>
+    </section>
 
 
-<section class="sx-section">
-    <div class="container">
-        <?= $this->render("@app/views/include/bottom-block"); ?>
-    </div>
-</section>
+<?
+$contentNews = \skeeks\cms\models\CmsContent::find()->where(['code' => 'news'])->one();
+?>
+<? $widget = \skeeks\cms\cmsWidgets\contentElements\ContentElementsCmsWidget::beginWidget('home-news', [
+    'enabledPaging'   => \skeeks\cms\components\Cms::BOOL_N,
+    'enabledRunCache' => \skeeks\cms\components\Cms::BOOL_Y,
+    'limit'           => 12,
+    'content_ids'     => [
+        $contentNews ? $contentNews->id : "",
+    ],
+    'viewFile'        => '@app/views/widgets/ContentElementsCmsWidget/news-masonry',
+]);
+
+
+if ($widget->dataProvider->query->count()) :
+    ?>
+    <section class="sx-section" style="padding-top: 3rem;">
+        <div class="container sx-container">
+            <?php $widget::end(); ?>
+        </div>
+    </section>
+<? else:  ?>
+    <?php $widget::end(); ?>
+<? endif; ?>
+
+
 <? if (!\Yii::$app->mobileDetect->isMobile) : ?>
-<section class="sx-section" style="padding: 0;">
-    <?= $this->theme->yandex_map; ?>
-</section>
+    <section class="sx-section" style="padding: 0;">
+        <?= $this->theme->yandex_map; ?>
+    </section>
 <? endif; ?>
